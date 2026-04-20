a1dcefa8b73cbb59a51e193f8e9064b025c424c2
Mpr6203 <22315a6203@sreenidhi.edu.in>
2026-04-20 14:30:27 +0530

    add whitenoise

--- khata_project2/settings.py
+++ khata_project2/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'khata_project2.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import dj_database_url
+import os
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
